767a5d28eb0b45239c5eebc6105bfc8a7943b0d5
WangHuancheng <wangkang980930@outlook.com>
2019-06-04 23:51:18 +0800

    silencer move test

---
+++ Silence-Initiative/Assets/Script/SilencerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SilencerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float Speed;
+    void Start()
+    {
+        Speed = 1f;//max speed in unit per second
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaXFromInput = Input.GetAxis("Horizontal");
+        float deltaYFromInput = Input.GetAxis("Horizontal");
+        Vector3 position = Vector3.zero;
+        const float Y_CorrectValue = 1.4142f;
+        position.x = deltaXFromInput * Speed * Time.deltaTime;
+        position.y = deltaXFromInput * Speed * Time.deltaTime * Y_CorrectValue;
+        transform.position += position;
+    }
+}
