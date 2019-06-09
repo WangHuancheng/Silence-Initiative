07f0af38257d21550b2a6fce32d9e591594d3296
WangHuancheng <wangkang980930@outlook.com>
2019-06-10 00:11:32 +0800

    add rigidbody to silencer

--- Silence-Initiative/Assets/Script/SilencerMove.cs
+++ Silence-Initiative/Assets/Script/SilencerMove.cs
@@ -16,6 +16,7 @@ public class SilencerMove : MonoBehaviour
     void Update()
     {
         Vector3 position = Vector3.zero;
+        Vector3 rigidbodyPosition = GetComponent<Rigidbody2D>().position;
         position.x = Input.GetAxis("Horizontal");
         position.z = Input.GetAxis("Vertical");
         position.Normalize();
@@ -23,7 +24,7 @@ public class SilencerMove : MonoBehaviour
         position.x *= Speed*Time.deltaTime;
         position.z *= Speed*Time.deltaTime*z_CorrectValue;
         //Debug.Log(position);
-        transform.position += position;
+        rigidbodyPosition += position;
         SilencerPositionChange = position;
     }
 }
