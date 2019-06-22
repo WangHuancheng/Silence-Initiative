e9c032cb176c1f4447c192dcfd7b55d9d7bbfdf2
WangHuancheng <wangkang980930@outlook.com>
2019-06-23 01:11:32 +0800

    FUCKED silencer shot

--- Silence-Initiative/Assets/Script/SilencerShot.cs
+++ Silence-Initiative/Assets/Script/SilencerShot.cs
@@ -16,9 +16,16 @@ public class SilencerShot : MonoBehaviour
     {
         float RotationAngle = 0f;
         Vector3 currentGunVector = transform.position;
-        Vector3 desiredGunVector = gameCursor.GetCursorWorldPosition(Camera.main);
+        Vector3 cursorWorldPosition = gameCursor.GetCursorWorldPosition(Camera.main);
+        cursorWorldPosition.z= cursorWorldPosition.y;
+        Vector3 desiredGunVector = cursorWorldPosition - currentGunVector;
+        desiredGunVector.z = -desiredGunVector.y;
         RotationAngle = FromToRotationAngle(currentGunVector,desiredGunVector);
-        transform.rotation = Quaternion.Euler(new Vector3(0,0,RotationAngle));
+        //transform.rotation = Quaternion.Euler(new Vector3(0,0,RotationAngle));
+        Vector3 rotationVector =  new Vector3(0,0,RotationAngle-55);//55 is a fucking magic number, MUSTN'T CHANGE IT
+        transform.localEulerAngles=rotationVector;
+        //Debug.Log("currentGunVector"+currentGunVector);
+        //Debug.Log("derierd"+desiredGunVector);
     }
 
     float FromToRotationAngle(Vector3 v1, Vector3 v2)
