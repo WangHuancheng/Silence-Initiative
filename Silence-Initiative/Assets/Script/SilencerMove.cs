8271dac2da8c8cfe89b22028d5708bc71abedd03
WangHuancheng <wangkang980930@outlook.com>
2019-08-05 00:27:58 +0800

    build v0.01
    finshi turning and try to build a shit

--- Silence-Initiative/Assets/Script/SilencerMove.cs
+++ Silence-Initiative/Assets/Script/SilencerMove.cs
@@ -9,6 +9,7 @@ public class SilencerMove : MonoBehaviour
     public Vector2 SilencerPositionChange;
     Rigidbody2D m_Rigidbody;
     Animator m_Animator;
+    bool isSilencerCurrentLeft = false;
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody2D>();
@@ -30,19 +31,26 @@ public class SilencerMove : MonoBehaviour
             MoveAnima(MoveInput);
         }
     }
-    void MovePhysic(Vector2 position)
+    void MovePhysic(Vector2 MoveInput)
     {
-        position.Normalize();
-        float y_CorrectValue = 1.414f;//due to our stupid fucking Coordinate,we need a correct value
-        position.x *= Speed*Time.deltaTime;
-        position.y *= Speed*Time.deltaTime*y_CorrectValue;
-        m_Rigidbody.MovePosition(position+m_Rigidbody.position);
-        transform.position = m_Rigidbody.position;
-        SilencerPositionChange = position;
+        Vector2 deltaPosition = Vector2.zero;
+        deltaPosition.x =  MoveInput.x * Speed * Time.deltaTime;
+        m_Rigidbody.MovePosition(deltaPosition+m_Rigidbody.position);
+        //transform.position = m_Rigidbody.position;
+        SilencerPositionChange = deltaPosition;//  editor中显示位置变化量
     }
     void MoveAnima(Vector2 MoveInput)
     {
-        m_Animator.SetFloat("WalkY",MoveInput.y);
+        if(MoveInput.x > 0 && isSilencerCurrentLeft==true)
+        {
+            transform.eulerAngles = new Vector3 (0,0,0);
+            isSilencerCurrentLeft = false;
+        }
+        else if(MoveInput.x < 0 && isSilencerCurrentLeft==false)
+        {
+            transform.eulerAngles = new Vector3 (0,180,0);
+            isSilencerCurrentLeft = true;
+        }
         m_Animator.SetFloat("WalkX",MoveInput.x);
     }
 
